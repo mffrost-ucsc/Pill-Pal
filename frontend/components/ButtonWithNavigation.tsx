756672833ed96f96fbd1e4df599a65137a6c4641
Andrew Katzfey <79179478+aKatzfey@users.noreply.github.com>
2024-05-06 12:54:39 -0700

    Add ButtonWithNavigation.tsx

---
+++ frontend/components/ButtonWithNavigation.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const NavigateButton: React.FC = ({routeName}) => {
+  const navigation = useNavigation();
+
+  const handlePress = () => {
+    console.log('pressing button')
+    navigation.navigate(routeName); // Replace 'NewScreen' with the name of your new screen
+  };
+
+  return (
+    <Button
+      title="Go to Medication Add Screen"
+      onPress={handlePress}
+      style={styles.button}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    marginVertical: 10,
+    padding: 10,
+  },
+});
+
+export default NavigateButton;
