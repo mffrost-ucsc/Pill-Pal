c7a57021b3a75aad5d574b1ea45d7966362e0308
Andrew Katzfey <79179478+aKatzfey@users.noreply.github.com>
2024-05-06 12:56:17 -0700

    LoginScreen.tsx

---
+++ frontend/components/LoginScreen.tsx
@@ -0,0 +1,73 @@
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+} from 'react-native';
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {RealmProvider} from '@realm/react';
+
+import Section from './Section'
+import NavigateButton from './ButtonWithNavigation';
+import LoginButton from './LoginButton';
+import Logo from '../pillHome.png';
+import {User} from '../realm/models';
+import { CreateUser } from './CreateUser';
+
+const styles = StyleSheet.create({
+  logo: {
+      width: 200,
+      height: 200,
+  },
+});
+
+function LoginScreen(){
+
+    const isDarkMode = useColorScheme() === 'dark';
+
+    const backgroundStyle = {
+        backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+        justifyContent: 'center',
+        alignItems: 'center',
+      };
+
+
+    return(
+     <SafeAreaView style={backgroundStyle}>
+                <StatusBar
+                  barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+                  backgroundColor={backgroundStyle.backgroundColor}
+                />
+                    <Image style={styles.logo} source={require('../pillHome.png')}/>
+                    <RealmProvider schema={[User]}>
+                                              <ScrollView
+                                                contentContainerStyle={{
+                                                  rowGap: 16
+                                                }}
+                                              >
+                    <CreateUser/>
+                    <LoginButton routeName={'HomeScreen'}/>
+                    </ScrollView>
+                    </RealmProvider>
+
+              </SafeAreaView>
+        );
+}
+
+
+export default LoginScreen
