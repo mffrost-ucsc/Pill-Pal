93979371c432afa5d2aa3f21e0c7be55ad02c70b
James Hill <jhill1@ucsc.edu>
2024-05-11 13:15:06 -0700

    adding servercode

---
+++ database/init/schema.sql
@@ -0,0 +1,40 @@
+-- Database Schema Should Go Here
+-- Code below right now is just for a test, it can be deleted/altered as needed
+
+
+CREATE DATABASE PillPall;
+USE PillPall;
+
+CREATE TABLE Patients (
+    PatientID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    PASSWORDS VARCHAR(50),
+    Phone VARCHAR(50),
+    BirthDate DATE,
+);
+
+CREATE TABLE Doctors (
+    DoctorID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(50),
+    Phone VARCHAR(50),
+);
+
+CREATE TABLE Prescriptions (
+    PrescriptionID INT PRIMARY KEY,
+    PatientID INT,
+    DoctorID INT,
+    PrescriptionDate DATE,
+    FOREIGN KEY (PatientID) REFERENCES Patients(PatientID),
+    FOREIGN KEY (DoctorID) REFERENCES Doctors(DoctorID),
+);
+
+CREATE TABLE Reminders (
+    ReminderID INT PRIMARY KEY,
+    PrescriptionID INT,
+    ReminderDate DATE,
+    FOREIGN KEY (PrescriptionID) REFERENCES Prescriptions(PrescriptionID)
+);
